91a009e17361e025bc972a9925fa89ae1ed4d3cf
Salsya Bunga Novita Putri <EMAIL_GITHUB_KAMU>
2026-08-20 11:31:14 +0700

    LKM-2 tata letak

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: const Center(child: Text('Kasir Koperasi')),
-      ),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Buku Tulis'),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Anggota: Rp3.000'),
+                  Text('Umum: Rp3.500'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),  
     );
   }
 }
